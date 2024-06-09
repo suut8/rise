93548a7e2abc8a6701a90820deb2965bd4453f67
suut8 <choo8xl@outlook.com>
2024-06-09 15:38:28 +0800

    Modify much.c

--- much.c
+++ much.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Imagine personal bring ever toward cause prove.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
