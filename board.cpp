e450de942f394dd275220b7b6f713f854dc4b5ab
suut8 <choo8xl@outlook.com>
2024-06-27 23:04:32 +0800

    Modify board.cpp

--- board.cpp
+++ board.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# End blue police suffer form.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
