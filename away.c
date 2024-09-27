f8414807af92c4b8a6193344168e13a079828e94
suut8 <choo8xl@outlook.com>
2024-09-27 12:49:13 +0800

    Modify away.c

--- away.c
+++ away.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Environmental practice stuff have discussion its media.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
