401f8145607856f234f652aaf685e403ec2fb1d7
suut8 <choo8xl@outlook.com>
2024-09-29 13:39:09 +0800

    Modify nothing.c

--- nothing.c
+++ nothing.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Conference season after win compare second.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
