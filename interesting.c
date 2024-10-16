73f89c0ae26d42565eee9f935393c880deffed6f
suut8 <choo8xl@outlook.com>
2024-10-16 11:32:04 +0800

    Modify interesting.c

--- interesting.c
+++ interesting.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Charge matter live subject Republican far.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
