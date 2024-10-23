de1fc59ab5a462f62b382fb2e674a546ce544ffa
suut8 <choo8xl@outlook.com>
2024-10-23 12:58:50 +0800

    Modify great.c

--- great.c
+++ great.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Bad standard responsibility physical baby board example fast.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
