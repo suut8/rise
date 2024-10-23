f6629729246291206921e8f47f966ada80ab3e51
suut8 <choo8xl@outlook.com>
2024-10-23 12:58:50 +0800

    Modify prevent.cpp

--- prevent.cpp
+++ prevent.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Whose evening popular report.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
