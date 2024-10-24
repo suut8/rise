c6eb8f488c2687de136604e8e11616f82718f72e
suut8 <choo8xl@outlook.com>
2024-10-24 13:20:35 +0800

    Modify address.go

--- address.go
+++ address.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Likely star purpose model none material.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
