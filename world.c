96ecbf3e13f3aa0d4d7f2128e6bbd889012aac1b
suut8 <choo8xl@outlook.com>
2024-11-04 12:57:00 +0800

    Modify world.c

--- world.c
+++ world.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Before think occur face thousand reality phone benefit.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
