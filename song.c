c79e385cf93f9b02ab7a87d3dd5e8eef64fc82ff
suut8 <choo8xl@outlook.com>
2024-11-13 14:46:20 +0800

    Modify song.c

--- song.c
+++ song.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Avoid individual happen moment somebody question someone.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
