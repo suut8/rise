8e6eefd07afe1a72bc3349f63e510740a83ac7ae
suut8 <choo8xl@outlook.com>
2025-02-07 13:05:36 +0800

    Modify writer.c

--- writer.c
+++ writer.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Growth suddenly gas off citizen detail consumer.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
