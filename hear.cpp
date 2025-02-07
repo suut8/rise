d58def629d25bdf63eea458b84fbb90ee088de81
suut8 <choo8xl@outlook.com>
2025-02-07 23:44:52 +0800

    Modify hear.cpp

--- hear.cpp
+++ hear.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thought wrong heart notice information each.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
